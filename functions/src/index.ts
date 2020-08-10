da17d60d81997c92276db70e9bebd99f74ff34a0
Hugo <hugofroidevaux89@gmail.com>
2020-08-10 16:25:57 -0300

    GetPersona

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,10 +5,17 @@ import * as functions from 'firebase-functions';
 
 const cors = require('cors')({ origin: true });
 const dialogflow = require('dialogflow');
+const { WebhookClient, Card } = require('dialogflow-fulfillment');
+const admin = require('firebase-admin');
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'ws://chatbot-1-13fa1.firebaseio.com/',
+});
 const client = new dialogflow.v2.SessionsClient({
     // optional auth parameters.
 });
 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 export const dialogflowGateway = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", { structuredData: true });
@@ -33,3 +40,38 @@ export const dialogflowGateway = functions.https.onRequest((request, response) =
     })
 
 });
+
+export const dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+    const agent = new WebhookClient({ request: request, response: response });
+
+    const db = admin.firestore();
+    const email = request.body.queryResult.parameters['email'];
+
+    const getPersona = () => {
+        const personaRead = db.collection('personas').where('email', '==', email).get();
+
+        return personaRead.then((snapshot: any) => {
+            agent.add('La información que he encontrado es la siguiente: ');
+
+            snapshot.forEach((doc: any) => {
+                const data = doc.data();
+                agent.add(new Card({
+                    title: data.nombre + " " + data.apellido,
+                    imageUrl: data.imageUrl,
+                    text: data.email + '\n' + data.fechaNacimiento,
+                    // buttonText: 'This is a button',
+                    // buttonUrl: 'https://assistant.google.com/'
+                }))
+            });
+
+        }).catch((err: any) => {
+            console.log('Error al obtener el documento', err);
+        });
+    };
+
+    const intentMap = new Map();
+    intentMap.set('GetInfo.Persona', getPersona);
+    agent.handleRequest(intentMap);
+
+});
+
