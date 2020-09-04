e7f84146e80b339d8f305092b78c5b0eabab2ed2
Cristian D Biancotti <cristian@mvzn.me>
2020-09-04 04:36:52 -0300

    Probando nueva interfaz del chat

--- chatbot-angular/src/app/app.module.ts
+++ chatbot-angular/src/app/app.module.ts
@@ -1,48 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbChatModule,
-  NbSpinnerModule, NbDialogModule, NbDatepickerModule, NbButtonModule, NbInputModule, NbCardModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { NbDateFnsDateModule } from '@nebular/date-fns';
-import { ChatbotComponent } from './chatbot/chatbot.component';
-import { LoginComponent } from './account/login.component';
-import { AppRouteGuard } from './account/auth/auth-route-guard';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbChatModule,
+  NbSpinnerModule,
+  NbDialogModule,
+  NbDatepickerModule,
+  NbButtonModule,
+  NbInputModule,
+  NbCardModule,
+} from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import { NbDateFnsDateModule } from "@nebular/date-fns";
+import { ChatbotComponent } from "./chatbot/chatbot.component";
+import { LoginComponent } from "./account/login.component";
+import { AppRouteGuard } from "./account/auth/auth-route-guard";
 
-import { FirebaseUIModule, firebase, firebaseui } from 'firebaseui-angular';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from 'src/environments/environment';
-import { DialogDatePromptComponent } from './dialog/dialog-date-prompt.component';
+import { FirebaseUIModule, firebase, firebaseui } from "firebaseui-angular";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { environment } from "src/environments/environment";
+import { DialogDatePromptComponent } from "./dialog/dialog-date-prompt.component";
+import { FormsModule } from "@angular/forms";
 
 const firebaseUiAuthConfig: firebaseui.auth.Config = {
-  signInFlow: 'popup',
+  signInFlow: "popup",
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     {
-      scopes: [
-        'public_profile',
-        'email',
-        'user_likes',
-        'user_friends'
-      ],
+      scopes: ["public_profile", "email", "user_likes", "user_friends"],
       customParameters: {
-        auth_type: 'reauthenticate'
+        auth_type: "reauthenticate",
       },
-      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     },
     {
       requireDisplayName: false,
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
     },
   ],
-  tosUrl: '<your-tos-link>',
-  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
-  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+  tosUrl: "<your-tos-link>",
+  privacyPolicyUrl: "<your-privacyPolicyUrl-link>",
+  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
 };
 
 @NgModule({
@@ -55,8 +60,9 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
+    NbThemeModule.forRoot({ name: "default" }),
     NbLayoutModule,
     NbEvaIconsModule,
     NbChatModule,
@@ -72,19 +78,15 @@ const firebaseUiAuthConfig: firebaseui.auth.Config = {
       autoFocus: true,
       hasScroll: false,
       closeOnEsc: false,
-      closeOnBackdropClick: false
+      closeOnBackdropClick: false,
     }),
     NbDatepickerModule.forRoot(),
     NbDateFnsDateModule.forRoot({
-      format: 'dd-MM-yyyy'
-    })
+      format: "dd-MM-yyyy",
+    }),
   ],
-  providers: [
-    AppRouteGuard
-  ],
+  providers: [AppRouteGuard],
   bootstrap: [AppComponent],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
--- chatbot-angular/src/app/chatbot/chatbot.component.ts
+++ chatbot-angular/src/app/chatbot/chatbot.component.ts
@@ -1,40 +1,48 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { finalize, catchError } from 'rxjs/operators';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { NbDialogService, NbSpinnerService } from '@nebular/theme';
-import { DialogDatePromptComponent } from '../dialog/dialog-date-prompt.component';
-import { environment } from 'src/environments/environment';
-import { Persona } from '../models/persona';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
+} from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { finalize, catchError } from "rxjs/operators";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { NbDialogService, NbSpinnerService } from "@nebular/theme";
+import { DialogDatePromptComponent } from "../dialog/dialog-date-prompt.component";
+import { environment } from "src/environments/environment";
+import { Persona } from "../models/persona";
 
 @Component({
-  selector: 'app-chatbot',
-  templateUrl: './chatbot.component.html',
-  styleUrls: ['./chatbot.component.scss']
+  selector: "app-chatbot",
+  templateUrl: "./chatbot.component.html",
+  styleUrls: ["./chatbot.component.scss"],
 })
 export class ChatbotComponent implements OnInit, AfterViewInit {
-
   messages = [];
   payloads: any;
 
   loading = false;
+  message = "";
+  dateBirth = "";
 
-  dateBirth = '';
-
-  userImageURL = '';
-  userName = 'Tú';
+  userImageURL = "";
+  userName = "Tú";
 
   // Random ID to maintain session with server
   sessionId = Math.random().toString(36).slice(-5);
+
+  @ViewChild("msgBox") msgBox: ElementRef;
 
   constructor(
     private http: HttpClient,
     private afAuth: AngularFireAuth,
     private dialogService: NbDialogService,
-    private spinnerService: NbSpinnerService) { }
+    private spinnerService: NbSpinnerService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
     try {
@@ -44,11 +52,16 @@ export class ChatbotComponent implements OnInit, AfterViewInit {
     }
   }
 
+  private scrollToBottom() {
+    this.msgBox.nativeElement.scrollTop = this.msgBox.nativeElement.scrollHeight;
+  }
   handleUserMessage(event) {
+    event.preventDefault();
     console.log(event);
-    const text = event.message;
+    const text = this.message;
+    this.message = "";
     this.addUserMessage(text);
-
+    this.scrollToBottom();
     this.loading = true;
 
     const bodyRequest = {
@@ -60,43 +73,61 @@ export class ChatbotComponent implements OnInit, AfterViewInit {
         // },
         text: {
           text,
-          languageCode: 'es'
-        }
-      }
+          languageCode: "es",
+        },
+      },
     };
 
     // Make an HTTP Request
-    this.http.post<any>(environment.dialogflowURL, bodyRequest)
-      .pipe(finalize(() => { this.loading = false; }))
-      .pipe(catchError((err, caught): any => {
-        console.log(err);
-      }))
-      .subscribe(res => {
+    this.http
+      .post<any>(environment.dialogflowURL, bodyRequest)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .pipe(
+        catchError((err, caught): any => {
+          console.log(err);
+        })
+      )
+      .subscribe((res) => {
         if (res) {
-          const respuestaTipoTexto = res.queryResult.fulfillmentMessages.filter(m => m.message === 'text');
-          const respuestaTipoPayload = res.queryResult.fulfillmentMessages.filter(m => m.message === 'payload');
+          const respuestaTipoTexto = res.queryResult.fulfillmentMessages.filter(
+            (m) => m.message === "text"
+          );
+          const respuestaTipoPayload = res.queryResult.fulfillmentMessages.filter(
+            (m) => m.message === "payload"
+          );
 
-          respuestaTipoTexto.forEach(mensaje => {
+          respuestaTipoTexto.forEach((mensaje) => {
             this.addBotMessage(mensaje.text.text[0]);
           });
 
           this.addBotPayLoad(respuestaTipoPayload);
-
+          setTimeout(() => this.scrollToBottom(), 0);
         }
       });
   }
 
   altaUsuario(parametros) {
     const bodyRequest = {
-      params: parametros
+      params: parametros,
     };
 
-    this.http.post<any>(environment.altaUsuarioURL, bodyRequest)
-      .pipe(finalize(() => { this.loading = false; }))
-      .pipe(catchError((err, caught): any => {
-        console.log(err);
-      }))
-      .subscribe(res => {
+    this.http
+      .post<any>(environment.altaUsuarioURL, bodyRequest)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .pipe(
+        catchError((err, caught): any => {
+          console.log(err);
+        })
+      )
+      .subscribe((res) => {
         if (res) {
           console.log(res);
         }
@@ -104,65 +135,68 @@ export class ChatbotComponent implements OnInit, AfterViewInit {
   }
 
   initUsuario() {
-    this.afAuth.authState.subscribe(d => {
-
+    this.afAuth.authState.subscribe((d) => {
       this.userImageURL = d.photoURL;
       this.userName = d.displayName ? d.displayName : d.email;
 
       const bodyRequest = {
         params: {
-          email: d.email
-        }
+          email: d.email,
+        },
       };
 
-      this.http.post<any>(environment.checkUsuarioURL, bodyRequest)
-        .pipe(catchError((err, caught): any => {
-          console.log(err);
-        }))
-        .subscribe(res => {
+      this.http
+        .post<any>(environment.checkUsuarioURL, bodyRequest)
+        .pipe(
+          catchError((err, caught): any => {
+            console.log(err);
+          })
+        )
+        .subscribe((res) => {
           if (!res) {
-            this.dialogService.open(DialogDatePromptComponent).onClose.subscribe(date => {
-              const params: Persona = {
-                displayName: d.displayName,
-                email: d.email,
-                fechaNacimiento: date,
-                imageURL: d.photoURL,
-                debePrimeraEntrega: true
-              };
-              this.altaUsuario(params);
-            });
+            this.dialogService
+              .open(DialogDatePromptComponent)
+              .onClose.subscribe((date) => {
+                const params: Persona = {
+                  displayName: d.displayName,
+                  email: d.email,
+                  fechaNacimiento: date,
+                  imageURL: d.photoURL,
+                  debePrimeraEntrega: true,
+                };
+                this.altaUsuario(params);
+              });
           }
         });
-
     });
   }
 
   async askDateOfBirth() {
-    return this.dialogService.open(DialogDatePromptComponent)
-      .onClose.subscribe(date => {
+    return this.dialogService
+      .open(DialogDatePromptComponent)
+      .onClose.subscribe((date) => {
         this.dateBirth = date;
       });
   }
-
 
   // Helpers
 
   addUserMessage(text) {
     this.messages.push({
       text,
-      sender: 'Tú',
+      sender: this.userName,
       reply: true,
       avatar: this.userImageURL,
-      date: new Date()
+      date: new Date(),
     });
   }
 
   addBotMessage(text) {
     this.messages.push({
       text,
-      sender: 'Bot',
-      avatar: '/assets/esteban.jpg',
-      date: new Date()
+      sender: "Estebot",
+      avatar: "/assets/esteban.jpg",
+      date: new Date(),
     });
   }
 
@@ -170,11 +204,9 @@ export class ChatbotComponent implements OnInit, AfterViewInit {
     this.payloads = payload;
   }
 
-
   logout() {
     this.afAuth.signOut().then(() => {
       window.location.reload();
     });
   }
-
 }
