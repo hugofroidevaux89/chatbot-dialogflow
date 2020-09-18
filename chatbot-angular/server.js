068cc24d4afb620a77cf49d0678697cb10968282
Hugo <hugofroidevaux89@gmail.com>
2020-09-18 10:55:44 -0300

    Config para deploy en Heroku

---
+++ chatbot-angular/server.js
@@ -0,0 +1,20 @@
+const express = requiere('express');
+const app = express();
+const path = require('path');
+const forceSSL = function() {
+    return function (req, res, next){
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('host'), req.url].join('')
+            )
+        }
+        next();
+    }
+}
+app.use(express.static(__dirname + '/dist/heroku-deployment'));
+
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/heroku-deployment/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
